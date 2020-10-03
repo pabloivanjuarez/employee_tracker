61352978578f6bb6227bc11142dcc91ab5f75a27
pablito <pabloivanjuarez@gmail.com>
2020-10-03 14:35:06 -0700

    roles added

--- database/role.sql
+++ database/role.sql
@@ -2,11 +2,18 @@ USE empTracker;
 
 INSERT INTO roles (title, salary, department_id)
 VALUES 
-("Lead", 300000, 2),
-("Lead", 300000, 2),
-("Lead", 300000, 2),
-("Lead", 300000, 2),
-("Lead", 300000, 2),
-("Lead", 300000, 2)
+("Sales Manager", 120000, 1),
+("Sales Lead", 100000, 1),
+("Sales Person", 80000, 1),
+("Marketing Manager", 120000, 1),
+("Marketing Lead", 100000, 1),
+("Market Watcher", 100000, 1),
+("Tech Manager", 120000, 2),
+("Tech Lead", 100000, 2),
+("Engineer", 80000, 2),
+("Finace Manager", 120000, 3),
+("Finace Lead", 10000, 3),
+("Accountant", 80000, 3),
+("HR Manager", 120000, 4),
+("HR Assistant", 80000, 4),
 
--- fill out your tables
