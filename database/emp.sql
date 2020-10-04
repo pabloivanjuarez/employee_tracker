e9088db7fc48aac9a9e5cb380c6fbbc9b621cfd2
pablito <pabloivanjuarez@gmail.com>
2020-10-04 01:44:44 -0700

    roles and Employee seeds updated

--- database/emp.sql
+++ database/emp.sql
@@ -1,5 +1,18 @@
 USE empTracker;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 2), ("Jane", "Dough", 2, 3);
---fill out your tables
+VALUES 
+("John", "Johnson", 1, 1),
+("Juno", "Luna", 1, 1),
+("Joseph", "Grace", 1, 1),
+("Anne", "Schrew", 1, 1),
+("Abe", "Fitzner", 1, 1),
+("Albina", "Duran", 1, 1),
+("Dane", "Dough", 2, 2),
+("Drake", "Kerry", 2, 2),
+("Diane", "kurtz", 2, 2),
+("Patricia", "Payne", 3, 3),
+("Barney", "Greene", 3, 3),
+("Cleaver", "Kurdson", 3, 3),
+("Juan", "Santos", 4, 4),
+("Josue", "Mendiola", 4, 4);
