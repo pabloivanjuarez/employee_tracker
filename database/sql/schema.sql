049654e2d393c23789c18334a5dbf434de8be026
pablito <pabloivanjuarez@gmail.com>
2020-10-05 18:00:35 -0700

    schema.sql updated, role table colums created

--- database/sql/schema.sql
+++ database/sql/schema.sql
@@ -11,8 +11,8 @@ CREATE TABLE department (
 CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
-  SALARY DECIMAL NOT NULL,
-  department_id INT unsigned,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
   INDEX department_index(department_id),
   CONSTRAINT f_key_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE 
 );
@@ -21,10 +21,10 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT unsigned NOT NULL,
+  role_id INT NOT NULL,
   INDEX role_index(role_id),
   CONSTRAINT f_key_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
-  manager_id int unsigned,
+  manager_id int,
   INDEX manager_index(manager_id),
   CONSTRAINT f_key_manager_id FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
